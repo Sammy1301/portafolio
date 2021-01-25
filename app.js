95a4ccd6ce6bcdc5af2e590211dbe76f15c7b27e
Sammy <sammy1301m@gmail.com>
2021-01-25 16:56:56 -0400

    logre hacer la barra de navegacion responsive y algunos features mas pero aun falta perfeccionar la nav

--- app.js
+++ app.js
@@ -10,14 +10,26 @@ $(document).ready(function(){
         return false;
       });
       
-      //esto es para poner el bachground a la barra de navegacion cuando bajemos
+      //esto es para poner el background a la barra de navegacion cuando bajemos
       $(document).on("scroll", function() {
-        
           if(window.scrollY>=200){
             $(".navi").css({"background-color":"#191c1a"});
           }else{
             $(".navi").css({"background-color":"transparent"});
           }
-          
       });
-})
+      //Efecto para ver el background de las tarjetas de proyecto
+      $(".div.-imagen").mouseenter(function(){
+        $("div.div.-imagen > div.imagen-bk").css("display", "flex");
+      });
+      //para bajar y subir el menu de opciones
+      $("#menubtn").click(function() {
+        $(".menu").slideToggle(300);
+      });
+      //para subir el menu cuando este en modo responsive
+      $(".menu a").click(function(){
+        if (window.innerWidth <= 768) {
+          $(".menu").slideUp(300);  
+        }
+      });
+});
